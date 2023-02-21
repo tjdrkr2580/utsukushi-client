915381b22bb648f8a2797595231e798f0b395952
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-21 22:50:08 +0900

    feat | button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,22 @@ import { ButtonPropsType } from "@utils/types/index";
 import styled, { css } from "styled-components";
 
 const CustomButton = styled.button<ButtonPropsType>`
+  cursor: pointer;
   background-color: ${(props) => props.theme.primary};
   color: ${(props) => props.theme.textColor1};
+  border-radius: 0.8rem;
+  font-weight: 500;
+  letter-spacing: 0.05rem;
   ${(props) =>
     props.wh === "m" &&
     css`
-      width: 5rem;
-      height: 3rem;
+      width: 7rem;
+      height: 3.5rem;
+      font-size: 1.3rem;
     `}
+  &:hover {
+    filter: brightness(85%);
+  }
 `;
 
 const Button = ({ children, ...props }: ButtonPropsType) => {
---
+++ src/utils/styles/Minxin.tsx
@@ -0,0 +1,19 @@
+import { css } from "styled-components";
+
+export const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const boxShadow = css`
+  box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+    rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+`;
+
+export const absoluteCenter = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
